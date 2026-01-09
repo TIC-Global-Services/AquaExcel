a142db3491683c654f2fc559857761fd5c66adc7
praveen <praveenramesh5267@gmail.com>
2026-01-09 16:30:58 +0530

    image update

--- components/why-us/hero.tsx
+++ components/why-us/hero.tsx
@@ -7,13 +7,14 @@ const hero = () => {
   return (
       <section className="relative h-[860px] w-full bg-hero-bg overflow-hidden">
           {/* Background Image */}
-          <div className="absolute inset-0 -z-10">
+          <div className="absolute inset-0">
         <Image
           src={banner}
-          alt=""
+          alt="AquaExcel Product"
           fill
+          className="object-cover scale-110"
           loading="lazy"
-          className="object-cover"
+          quality={90}
         />
       </div>
     
@@ -21,17 +22,17 @@ const hero = () => {
           <div className="relative h-[860px] flex items-end pb-8">
             <div className="px-6 xl:px-[80px] lg:px-[40px] w-full">
               <div className="max-w-5xl">
-                <h1 className="text-hero-text font-hoves-pro font-medium text-[60px] leading-[64px] tracking-[-0.04em] mb-4">
+                <h1 className="text-hero-text font-hoves-pro font-medium xl:text-[60px] text-[22px] leading-[64px] tracking-[-0.04em] mb-4">
                 Strength in every detail,<br/>reliability in every moment of use.
                 </h1>
                 
-                <p className="text-hero-text font-hoves-pro font-light text-[20px] text-base max-w-4xl mb-4">
+                <p className="text-hero-text font-hoves-pro font-light xl:text-[20px] text-base max-w-4xl mb-4">
                   Trusted By Thousands Of Plumbers, Built On Patent-Applied Technology, Every Aqua Excel <br></br>Product Is Made To Last, Leak-Free And Worry-Free.
                 </p>
     
                 <div className="flex flex-wrap gap-4">
-                  <Button variant="primary">Explore Innovations</Button>
-                  <Button variant="secondary">Customer Enquiry</Button>
+                  <Button variant="primary" className='xl:text-[12px] text-[10px]'>Explore Innovations</Button>
+                  <Button variant="secondary" className='xl:text-[12px] text-[10px]'>Customer Enquiry</Button>
                 </div>
               </div>
             </div>
