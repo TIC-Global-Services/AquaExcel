848aabd21c37ebc2e00241eea304ef56c20fd60a
praveen <praveenramesh5267@gmail.com>
2026-06-13 13:19:37 +0530

    apply button update

--- components/carrer/Careeropportunities.tsx
+++ components/carrer/Careeropportunities.tsx
@@ -55,12 +55,11 @@ const Careeropportunities = () => {
           className="object-contain md:hidden block -rotate-90 opacity-30 z-0 scale-[1.8] translate-x-[18%] translate-y-[2%]"
         />
         <div className="z-20 relative flex items-center  justify-center min-h-[400px] md:min-h-[80vh] lg:min-h-[75.111vh]">
-            <div className='bg-[#FAF9F5] flex flex-col items-center justify-center py-3 gap-5 py-[3%] px-6 rounded-[20px] max-w-3xl shadow-sm border-[1px]'>
-              <h2 className="text-black text-2xl font-semibold tracking-[-4%]">We’re hiring</h2>
+            <div className='bg-[#FAF9F5] flex flex-col items-center justify-center py-3 gap-5 py-[3%] px-6 rounded-[20px] max-w-xl shadow-sm border-[1px]'>
+              <h2 className="text-black text-4xl font-semibold tracking-tight">We’re Hiring</h2>
               {/* <input className='w-full rounded-xl border-[1px] py-[2%] px-5' placeholder='Enter your email'></input> */}
-               <p className='text-center'>Apply through our quick application form and select the role <br/> that best matches your skills and experience.</p>
-              <Link href={"https://docs.google.com/forms/d/e/1FAIpQLSe2H_LoDBTZaska9nO8oZ_q7AAMgHVbeUJaFDV_d4rmcsh4MQ/viewform?usp=publish-editor"} target="_blank"><button className="text-white bg-[#E31E24] font-medium text-sm px-5 py-2 rounded-xl">Apply now</button></Link>
-             
+               <p className='text-center text-xl'>Apply through our quick application form and select the role <br/> that best matches your skills and experience.</p>
+              <Link href={"https://docs.google.com/forms/d/e/1FAIpQLSe2H_LoDBTZaska9nO8oZ_q7AAMgHVbeUJaFDV_d4rmcsh4MQ/viewform?usp=publish-editor"} target="_blank"><button className="text-white bg-[#E31E24] font-medium text-base px-5 py-2 rounded-xl">Apply now</button></Link>
             </div>
         </div>
       </div>
